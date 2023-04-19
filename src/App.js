403871de3c36700cf4c70b43a07ffe5a667d9306
myatsusuhlaing <myatsusuhlaing04@gmail.com>
2023-04-19 13:57:44 +0630

    addin tailwind css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
 import Table from "./Table";
-import data from "./data"
 import './App.css';
 
 function DataTable(){
-  const getHeadings = () =>{
-    return Object.keys(data[0]);
-  }
-
   return(
     <div className="datatable">
       <h2>React Data Table</h2>
-      <Table tbodyData={data} theadData={getHeadings()} />
+      <Table />
     </div>
   )
 }
--- src/Table.js
+++ src/Table.js
@@ -1,27 +1,41 @@
 import React from "react";
-import './App.css';
+import data from "./data"
+import './input.css';
 
-export default function Table ({theadData,tbodyData}) {
+function Table () {
+
+    //table column
+    const column = Object.keys(data[0]);
+    //table heading
+    const headingData = () =>{
+        return column.map((headData)=>{
+            return <th scope="col" className="border-r px-6 py-4 dark:border-neutral-500" key={headData}>{headData}</th>
+        })
+    }
+    //table row
+    const rowsData = () =>{
+        return data.map((tableData)=>{
+            return(
+                <tr  className="border-r px-6 py-4 dark:border-neutral-500">
+                    {column.map((rowData)=>{
+                    return <td className="border-r px-6 py-4 dark:border-neutral-500 ">{tableData[rowData]}</td>
+                })}
+                </tr>
+            )
+        })
+    }
+
     return(
-        <div className="table">
-            <table>
-                <thead>
-                    <tr>{theadData.map(heading =>{
-                        return <th key={heading}> {heading}</th>
-                    })}
-                    </tr>
+        <div className="flex flex-col " >
+            <table className="min-w-full border text-center text-sm font-light dark:border-neutral-500">
+                <thead className="border-b font-medium dark:border-neutral-500">
+                    <tr className="text-blue-600/100 dark:border-neutral-500">{headingData()}</tr>
                 </thead>
-                <tbody>
-                    {tbodyData.map((row,index) =>{
-                    return <tr key={index}>
-                        {theadData.map((key,index)=>{
-                            return <td key={row[key]}>{row[key]}</td>
-                        })}
-                        </tr>
-                    })}
-                    
+                <tbody className="border-b font-medium dark:border-neutral-500">
+                   {rowsData()}
                 </tbody>
             </table>
         </div>
     )
 }
+export default Table;
