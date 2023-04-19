c513b63706f81676998885f93fd3c1ee46af32bc
myatsusuhlaing <myatsusuhlaing04@gmail.com>
2023-04-19 15:51:03 +0630

    table hover color

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 
 function DataTable(){
   return(
+    <>
     <div className="datatable">
       <h2>React Data Table</h2>
-      <Table />
     </div>
+    <div>
+        <Table />
+    </div>
+    </>
   )
 }
 export default DataTable;
--- src/Table.js
+++ src/Table.js
@@ -9,16 +9,16 @@ function Table () {
     //table heading
     const headingData = () =>{
         return column.map((headData)=>{
-            return <th scope="col" className="border-r px-6 py-4 dark:border-neutral-500" key={headData}>{headData}</th>
+            return <th scope="col" className="border md:bg-gray-200 ... px-6 py-4 border-gray-300 " key={headData}>{headData}</th>
         })
     }
     //table row
     const rowsData = () =>{
         return data.map((tableData)=>{
             return(
-                <tr  className="border-r px-6 py-4 dark:border-neutral-500">
+                <tr className="border hover:bg-gray-300 ... px-6 py-4 border-gray-300">
                     {column.map((rowData)=>{
-                    return <td className="border-r px-6 py-4 dark:border-neutral-500 ">{tableData[rowData]}</td>
+                    return <td className="border px-6 py-4 border-gray-300">{tableData[rowData]}</td>
                 })}
                 </tr>
             )
@@ -26,12 +26,12 @@ function Table () {
     }
 
     return(
-        <div className="flex flex-col " >
-            <table className="min-w-full border text-center text-sm font-light dark:border-neutral-500">
-                <thead className="border-b font-medium dark:border-neutral-500">
-                    <tr className="text-blue-600/100 dark:border-neutral-500">{headingData()}</tr>
+        <div className="m-8 ..." >
+            <table className="min-w-full border-gray-400">
+                <thead className="border-r font-medium border-gray-400">
+                    <tr className="border text-blue-500/100 text-left border-gray-400">{headingData()}</tr>
                 </thead>
-                <tbody className="border-b font-medium dark:border-neutral-500">
+                <tbody className="border font-medium text-left">
                    {rowsData()}
                 </tbody>
             </table>
