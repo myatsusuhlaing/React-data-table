9161d28ef0d2e92532215544334f5b71a638f2c6
myatsusuhlaing <myatsusuhlaing04@gmail.com>
2023-04-19 09:28:46 +0630

    react data table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
+import Table from "./Table";
+import data from "./data"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function DataTable(){
+  const getHeadings = () =>{
+    return Object.keys(data[0]);
+  }
 
-export default App;
+  return(
+    <div className="datatable">
+      <h2>React Data Table</h2>
+      <Table tbodyData={data} theadData={getHeadings()} />
+    </div>
+  )
+}
+export default DataTable;
