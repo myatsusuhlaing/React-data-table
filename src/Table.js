7c4a7999e3227a7c93205aea42c699953dfb5ebf
myatsusuhlaing <myatsusuhlaing04@gmail.com>
2023-04-19 23:06:36 +0630

    drop-down icon

--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,7 @@
 import React from "react";
 import data from "./data"
 import './input.css';
+import { ChevronDoubleDownIcon,ChevronDownIcon } from '@heroicons/react/20/solid';
 
 function Table () {
 
@@ -16,7 +17,8 @@ function Table () {
     const rowsData = () =>{
         return data.map((tableData)=>{
             return(
-                <tr className="border hover:bg-gray-300 ... px-6 py-4 border-gray-300">
+                <tr className="border hover:bg-gray-300 ... px-6 py-4 border-gray-300 ">
+                    <td><ChevronDownIcon className="h-6 w-6 tex-gray-300 justify-center items-center" /></td>
                     {column.map((rowData)=>{
                     return <td className="border px-6 py-4 border-gray-300">{tableData[rowData]}</td>
                 })}
@@ -28,8 +30,11 @@ function Table () {
     return(
         <div className="m-8 ..." >
             <table className="min-w-full border-gray-400">
-                <thead className="border-r font-medium border-gray-400">
-                    <tr className="border text-blue-500/100 text-left border-gray-400">{headingData()}</tr>
+                <thead className="border font-medium border-gray-400 md:bg-gray-200 ... ">
+                    <tr className="border text-blue-500/100 text-left border-gray-300">
+                        <ChevronDoubleDownIcon className="h-6 w-6 text-blue-500 justify-center items-center" />
+                        {headingData()}
+                    </tr>
                 </thead>
                 <tbody className="border font-medium text-left">
                    {rowsData()}
